4131186afe79ab2e0655a588fa5874951d2e2fd4
Emery Tanghanwaye <emerytang@gmail.com>
2019-11-26 06:05:11 +0300

    gta2swifi: add wcnss symlinks
    Signed-off-by: Ataberk Özen <ataberkozen123@gmail.com>
    Change-Id: I191fdfb08ce5c45df69bcd4bbcb35b743ac9972e

--- Android.mk
+++ Android.mk
@@ -176,4 +176,19 @@ ALL_DEFAULT_INSTALLED_MODULES += \
 	$(RFS_MSM_CDSP_SYMLINKS) \
 	$(RFS_MSM_MPSS_SYMLINKS) \
 	$(RFS_MSM_SLPI_SYMLINKS)
+
+# Wcnss
+WCNSS_IMAGES := \
+	wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 \
+	wcnss.b09 wcnss.b10 wcnss.b11 wcnss.b12 wcnss.mdt
+
+WCNSS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(WCNSS_IMAGES)))
+$(WCNSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_SYMLINKS)
+
 endif
