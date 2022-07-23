7287009132d5b005d6e31035a5a7df86840552a5
Han Sol Jin <hansol@hansol.ca>
2022-07-22 23:47:39 -0700

    Revert "Enable TARGET_COPY_OUT_VENDOR"
    We don't have a vendor partition.
    
    This reverts commit f60b08a0481c14807112376ca08a79a59931cf12.
    
    Change-Id: I505e418e58591b9fa845bc24c4bc6ac8b71875be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -123,7 +123,8 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /mnt/vendor/persist:/persist \
     /mnt/vendor/efs:/efs
 
-TARGET_COPY_OUT_VENDOR := vendor
+#TARGET_COPY_OUT_VENDOR := system/vendor
+#BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 
 # FM
 BOARD_HAS_QCA_FM_SOC := "cherokee"
