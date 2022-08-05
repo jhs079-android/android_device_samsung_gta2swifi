7ba2839fa2c7cbb755f925012238186f5447fc17
Han Sol Jin <hansol@hansol.ca>
2022-08-05 10:08:34 -0700

    gta2swifi: build camera provider/device from source
    Change-Id: Iedba4890d838bd453def8d440612d3f48f508669

--- device.mk
+++ device.mk
@@ -105,12 +105,10 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    camera.msm8937 \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    camera.device@3.3-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
     camera.device@3.4-impl \
-    camera.device@3.5-impl \
+    camera.msm8937 \
     libcamera_shim \
     Snap
 
