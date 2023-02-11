b8bfe0e1615d6836cd2e5b8287db67a4ab87a8cf
Han Sol Jin <hansol@hansol.ca>
2023-02-11 15:38:28 -0800

    gta2swifi: uprev DRM stack to 1.1
    Change-Id: I2a18c755cd42ed79e38ad3d05f127a5c71179878

--- device.mk
+++ device.mk
@@ -141,9 +141,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.clearkey
+    android.hardware.drm@1.1-impl \
+    android.hardware.drm@1.1-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
