a8cc8d8dd5b80246ca37d08e51a326d611488059
Han Sol Jin <hansol@hansol.ca>
2023-12-13 04:41:25 -0800

    gta2swifi: drop vendor security patch level
    This device wasn't, and isn't, Treble, so this shouldn't be here.
    
    Change-Id: I3c47317c3cf02caf9fcec80f275e6bd568797c34

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,9 +37,6 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a7
 
-# Security patch level
-VENDOR_SECURITY_PATCH := 2020-12-01
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_BASE := 0x80000000
