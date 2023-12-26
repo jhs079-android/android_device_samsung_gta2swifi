e59f2b53987c48b760e914716d8d5737af476ff1
Han Sol Jin <hansol@hansol.ca>
2023-12-25 22:39:42 -0800

    gta2swifi: initial ARM64 conversion
    Change-Id: Ib53bc19f59c8e76556b02efa548daec0dcd95519

--- lineage_gta2swifi.mk
+++ lineage_gta2swifi.mk
@@ -15,14 +15,15 @@
 #
 
 # inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit from gta2swifi device
-$(call inherit-product, device/samsung/gta2swifi/device.mk)
-
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit from gta2swifi device
+$(call inherit-product, device/samsung/gta2swifi/device.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1200
